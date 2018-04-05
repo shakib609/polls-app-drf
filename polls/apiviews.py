44c6bb432c3748c2a3f717bac20642f0dc68e007
Shakib Hossain <shakib609@gmail.com>
2018-04-06 03:23:56 +0600

    A little conditional refactoring

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -53,9 +53,7 @@ class CreateVote(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserCreate(generics.CreateAPIView):
